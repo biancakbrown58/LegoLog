b03431b08366755a5b0e09cebcdbb47b8fe0a067
Bianca Brown <biancakbrown58@gmail.com>
2021-06-28 16:59:26 -0400

    worked on styling

--- ClientApp/src/pages/AddWishList.jsx
+++ ClientApp/src/pages/AddWishList.jsx
@@ -2,6 +2,48 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 export function AddWishList() {
+  const params = useParams()
+  const id = params.id
+
+  const [newWish, setNewWish] = useState({
+    theme: '',
+    legos: [],
+  })
+
+  const [addLego, setAddLego] = useState({
+    name: '',
+    theme: '',
+    pieceCount: '',
+    serialNumber: '',
+    inProgress: false,
+    price: '',
+    comment: '',
+    rating: 0,
+    finishedLego: false,
+    buildListId: id,
+    wishListId: id,
+  })
+
+  useEffect(() => {
+    async function fetchWishList() {
+      const response = await fetch(`/api/WishLists/${id}`)
+      if (response.ok) {
+        const apiData = await response.json()
+        setNewWish(apiData)
+        // console.log(apiData.rating)
+      }
+    }
+    fetchWishList()
+  }, [id])
+
+  function handleNewReviewTextFieldChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+
+    setAddLego({ ...addLego, [name]: value })
+    // setNewBuild({ ...newBuild, [name]: value })
+  }
+
   return (
     <>
       <form>
